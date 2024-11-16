b1292fb3da7ce11ed5bd8bb0b560412342a6769b
acoggins2022 <acoggins2022@fau.edu>
2024-11-15 22:02:57 -0500

    Update MainInterface.java

--- src/main/java/GUI/MainInterface.java
+++ src/main/java/GUI/MainInterface.java
@@ -31,7 +31,7 @@ public class MainInterface {
         // Create the main frame
         frame = new JFrame("Planner Application");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
+        frame.setSize(1000, 1000);
         frame.setLayout(new BorderLayout());
 
         // Create the content panel with CardLayout
