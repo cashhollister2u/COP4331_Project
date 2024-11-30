d1645df3de0d89ab8f9b5f729e25852a9e23b37f
acoggins2022 <acoggins2022@fau.edu>
2024-11-30 13:20:50 -0500

    Update CalendarViewStrategy.java

--- src/main/java/cop4331/SharedViews/CalendarViewStrategy.java
+++ src/main/java/cop4331/SharedViews/CalendarViewStrategy.java
@@ -7,10 +7,16 @@ package cop4331.SharedViews;
 import javax.swing.*;
 
 /**
- *
+ * Interface for defining the strategy for creating different calendar views.
  * @author andrewcoggins
  */
 
 public interface CalendarViewStrategy {
+    /**
+     * Creates and returns a JPanel representing the calendar view.
+     *
+     * @param frame The JFrame to which the calendar view will be added.
+     * @return A JPanel representing the calendar view.
+     */
     JPanel createCalendarView(JFrame frame);
 }
